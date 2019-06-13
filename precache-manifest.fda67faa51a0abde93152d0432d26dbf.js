06e17d96319ef36b0e006e07c831f17550bb87d3
pkcnguyen@gmail.com <pkcnguyen@gmail.com>
2019-06-13 13:31:44 +0700

    Updates

---
+++ precache-manifest.fda67faa51a0abde93152d0432d26dbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f06f881c988e9afae68fb7897b676f9",
+    "url": "/first_reactNative/index.html"
+  },
+  {
+    "revision": "678530f6cc8c287d31ea",
+    "url": "/first_reactNative/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f533336e0b9db58e94eb",
+    "url": "/first_reactNative/static/js/2.4908cf39.chunk.js"
+  },
+  {
+    "revision": "678530f6cc8c287d31ea",
+    "url": "/first_reactNative/static/js/main.bb5fda82.chunk.js"
+  },
+  {
+    "revision": "4927d3d4685f6d689085",
+    "url": "/first_reactNative/static/js/runtime~main.2accc6c6.js"
+  }
+]);
